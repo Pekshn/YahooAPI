a371b110125ccf3423d3fac9ea71d5db38ee0f15
Petar <iosdev.petar@gmail.com>
2025-04-10 00:06:15 +0200

    Initial commit

---
+++ YahooAPI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  YahooAPI
+//
+//  Created by Petar  on 10. 4. 2025..
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ YahooAPI/YahooAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  YahooAPIApp.swift
+//  YahooAPI
+//
+//  Created by Petar  on 10. 4. 2025..
+//
+
+import SwiftUI
+
+@main
+struct YahooAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
